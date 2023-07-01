001cf51f18d42fbd8ef777c793af8ed4a30cf4b7
Federico Amato <federico.amato1998@gmail.com>
2023-07-01 07:49:10 +0200

    Better circuits and new gates

--- circuits.py
+++ circuits.py
@@ -16,6 +16,21 @@ class Gate:
         self.control_lane = control_lane
         self.controlled = True if control_lane is not None else False
 
+    @property
+    def used_systems(self):
+        lanes = [self.controller.lanes[lane] for lane in self.lanes]
+        systems = []
+        duplicated_systems = []
+        for lane in lanes:
+            if lane.system is not None and not lane.is_entangled():
+                systems.append(lane.system)
+            else:
+                # Append only lanes whose systems are not already in the list
+                if lane.system not in duplicated_systems:
+                    systems.append(lane.system)
+                    duplicated_systems.append(lane.system)
+        return systems
+
     @staticmethod
     def check(control_system):
         if control_system == -1:
@@ -27,8 +42,11 @@ class Gate:
         return system
 
     def inject(self, step, system):
+        next_step = step + 1
         for lane in self.lanes:
-            self.controller.lanes[lane].system_evolution[step + 1] = system
+            series = self.controller.lanes[lane].system_evolution
+            series[step + 1: ] = [system for _ in range(len(series) - next_step)]
+        return None
 
     def __str__(self):
         return f"{self.name} acting on {self.lanes}"
@@ -37,9 +55,8 @@ class Gate:
 class Lane:
     def __init__(self, controller, system: qutip.Qobj | int | None = None, name=''):
         self.controller = controller
-        self.system = system
         self.name = name
-        self._system_evolution = [system for _ in range(len(self.controller.gates) + 2)]
+        self._system_evolution = [system for _ in range(len(self.controller.gates) + 1)]
 
     @property
     def system_evolution(self):
@@ -51,7 +68,8 @@ class Lane:
 
     @property
     def system(self) -> qutip.Qobj | int | None:
-        return self._system
+        """Return current step system"""
+        return self._system_evolution[self.controller.step]
     
     @system.setter
     def system(self, value):
@@ -61,11 +79,14 @@ class Lane:
                     raise ValueError("Classical systems may only take +1 or -1 values")
             else:
                 raise ValueError("System in the circuit may only be qutip.Qobj objects or +1 or -1.")
-        self._system = value
+        self._system_evolution[self.controller.step] = value
         
     def is_entangled(self):
-        rows = self.system.dims[0]
-        columns = self.system.dims[1]
+        if isinstance(self.system, int):
+            return False
+        else:
+            rows = self.system.dims[0]
+            columns = self.system.dims[1]
         if len(rows) == 1 and len(columns) == 1:
             return False
         else:
@@ -93,8 +114,10 @@ class Circuit:
         self.gates = []
         self.lanes = [Lane(self) for _ in range(n_lanes)]
         self.gate_map = {}
-
-        self.logging = logging
+        self.step = 0
+        self.logging: bool = logging
+        self.results: dict = {}
+        self.t = 0
 
     @property
     def matrix(self):
@@ -180,27 +203,46 @@ class Circuit:
                     used_systems.append(current_system)
         return used_systems
 
-    def run(self):
-        for step, gate in enumerate(self.gates):
-            step += 1  # The first step is empty
-            used_lanes = [self.lanes[i] for i in gate.lanes]
-            systems = self._select_systems(step, used_lanes)
+    def evaluate_gate(self, gate, step):
+        try:
             # Entangle systems
-            total_system = qutip.tensor(systems)
-            if self.logging:
-                print(f"Step {step} - Gate {gate}")
-                print(f"Used lanes: {used_lanes}")
-                print(f"{'_'*40}\n")
-            if not gate.controlled:
+            total_system = qutip.tensor(gate.used_systems)
+        except TypeError:
+            total_system = gate.used_systems
+        if self.logging:
+            print(f"Step {step} - Gate {gate}")
+            print(f"Used lanes: {[self.lanes[i] for i in gate.lanes]}")
+            print(f"{'_' * 40}\n")
+        if not gate.controlled:
+            total_system = gate.evolve(total_system)
+            gate.inject(step, total_system)
+        else:
+            control_system = self.lanes[gate.control_lane].system
+            if gate.check(control_system):
                 total_system = gate.evolve(total_system)
                 gate.inject(step, total_system)
-            else:
-                control_system = self.lanes[gate.control_lane].system
-                if gate.check(control_system):
-                    total_system = gate.evolve(total_system)
-                    gate.inject(step, total_system)
+        return total_system
+
+    def run(self):
+        for step, gate in enumerate(self.gates):
+            self.step = step
+            self.evaluate_gate(gate, step)
         return None
-        
+
+    def loop(self, times, start=0, stop=-1):
+        stop = len(self.gates) if stop == -1 else stop
+        loop_gates = self.gates[start: stop]
+        for t in range(times):
+            self.t += 1
+            for step, gate in enumerate(loop_gates):
+                self.step = step + start
+                self.evaluate_gate(gate, step)
+            # Replace starting systems with evolved ones
+            for lane in self.lanes:
+                evolved_system = lane.system_evolution[self.step + 1]
+                lane.system_evolution[start:] = [evolved_system for _ in range(len(self.gates) + 1 - start)]
+        return None
+
     def draw(self):
         columns = len(self.gates) + 1
         ascii_string = ''
