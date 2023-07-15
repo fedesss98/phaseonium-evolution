0f1e57254d4e89f45d3959302a9122a92dafb9e9
Federico Amato <federico.amato1998@gmail.com>
2023-07-15 17:15:39 +0200

    Add new folders

---
+++ src/physics.py
@@ -0,0 +1,66 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from qutip import Qobj
+import os
+from tqdm import tqdm, trange
+
+
+def _dag(x: Qobj | np.ndarray):
+    if isinstance(x, Qobj):
+        return x.dag()
+    elif isinstance(x, np.ndarray):
+        return x.conj().T
+
+
+def dag(x):
+    if isinstance(x, (Qobj, np.ndarray)):
+        return _dag(x)
+    elif isinstance(x, list):
+        return [_dag(op) for op in x]
+
+
+def commutator(a: Qobj | np.ndarray, b: Qobj | np.ndarray, kind='regular'):
+    if isinstance(a, Qobj) and isinstance(b, Qobj):
+        if kind == 'regular':
+            return a * b - b * a
+        elif kind == 'anti':
+            return a * b + b * a
+    elif isinstance(a, np.ndarray) and isinstance(b, np.ndarray):
+        if kind == 'regular':
+            return a @ b - b @ a
+        elif kind == 'anti':
+            return a @ b + b @ a
+    else:
+        raise TypeError('a and B must be of the same type')
+
+
+def dissipator(x: Qobj | np.ndarray, system: Qobj | np.ndarray, kind='regular'):
+    if kind == 'regular':
+        if isinstance(system, Qobj):
+            sandwich = x * system * dag(x)
+        else:
+            sandwich = x @ system @ dag(x)
+    elif kind == 'anti':
+        if isinstance(system, Qobj):
+            sandwich = dag(x) * system * x
+        else:
+            sandwich = dag(x) @ system @ x
+    else:
+        raise ValueError('Dissipator kind must be "regular" or "anti"')
+    xdx = dag(x) @ x if isinstance(x, np.ndarray) else dag(x) * x
+    comm = commutator(xdx, system, kind='anti')
+    return sandwich - 1/2 * comm
+
+
+def master_equation(system, ga, gb, operators):
+    c, cp, s, sd = operators
+
+    # Dissipators
+    d_cc_2ssd = dissipator(np.kron(c, c) - 2*np.kron(s, sd), system)
+    d_cs_scp = dissipator(np.kron(c, s) + np.kron(s, cp), system)
+    first_line = 0.5 * d_cc_2ssd + d_cs_scp
+
+    d_cpcp_2sds = dissipator(np.kron(cp, cp) - 2*np.kron(sd, s), system)
+    d_cp_sd = dissipator(np.kron(cp, sd) + np.kron(sd, c), system)
+    second_line = 0.5 * d_cpcp_2sds + d_cp_sd
+    return ga * first_line + gb * second_line
