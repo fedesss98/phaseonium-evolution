f37a4ba35d1d238fa247158c2b89f8ad93b4eb74
Federico Amato <federico.amato1998@gmail.com>
2023-07-18 13:33:31 +0200

    Add covariance matrix observables confrontation

--- src/observables.py
+++ src/observables.py
@@ -11,8 +11,10 @@ def calc_eignevalues(data: np.ndarray):
 
 
 def entropy_vn(rho: np.ndarray, base=np.e, fast=True):
+    """ Tr[rho log(rho)] done by diagonalizing the density matrix"""
     vals = calc_eignevalues(rho)
     nzvals = vals[vals != 0]  # Not-zero eigenvalues
+    # Choose function to use
     if base == 2:
         log = np.log2 if fast else np.lib.scimath.log2
     elif base == np.e:
@@ -38,4 +40,42 @@ def covariance(rho: np.ndarray, operators):
          for k in range(len(operators))]
         for l in range(len(operators))
     ]
-    return cov
+    return np.array(cov)
+
+
+def covariance_invariants(covariance_matrix: np.ndarray):
+    """Ferraro, Olivares, Paris - 2005; pag.21"""
+    a = covariance_matrix[0:2, 0:2]
+    b = covariance_matrix[2:4, 2:4]
+    c = covariance_matrix[0:2, 2:4]
+    i1 = np.linalg.det(a)
+    i2 = np.linalg.det(b)
+    i3 = np.linalg.det(c)
+    i4 = np.linalg.det(covariance_matrix)
+    return [i1, i2, i3, i4]
+
+
+def symplectic_eigenvalues(covariance_matrix: np.ndarray):
+    """Ferraro, Olivares, Paris - 2005; pag.22"""
+    i1, i2, i3, i4 = covariance_invariants(covariance_matrix)
+    first_factor = i1 + i2 + 2 * i3
+    second_factor = np.sqrt(first_factor ** 2 - 4 * i4)
+    d1 = np.sqrt((first_factor + second_factor) / 2)
+    d2 = np.sqrt((first_factor - second_factor) / 2)
+    return d1, d2
+
+
+def minientropy(d):
+    """Ferraro, Olivares, Paris - 2005; pag.22"""
+    x1 = d + 1/2
+    x2 = d - 1/2
+    return x1*np.log(x1) - x2*np.log(x2)
+
+
+def symplectic_entropy(nus):
+    """Ferraro, Olivares, Paris - 2005; pag.22"""
+    return sum([minientropy(nu) for nu in nus if nu != 0])
+
+
+def symplectic_purity(nus):
+    return np.prod([1/nu for nu in nus])
