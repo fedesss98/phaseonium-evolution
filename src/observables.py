cee1235724c09050a73d4f3541f5ef8c0f059373
Federico Amato <federico.amato1998@gmail.com>
2023-07-19 08:11:26 +0200

    Add advanced observables

--- src/observables.py
+++ src/observables.py
@@ -45,12 +45,12 @@ def covariance(rho: np.ndarray, operators):
 
 def covariance_invariants(covariance_matrix: np.ndarray):
     """Ferraro, Olivares, Paris - 2005; pag.21"""
-    a = covariance_matrix[0:2, 0:2]
-    b = covariance_matrix[2:4, 2:4]
-    c = covariance_matrix[0:2, 2:4]
-    i1 = np.linalg.det(a)
-    i2 = np.linalg.det(b)
-    i3 = np.linalg.det(c)
+    alpha = covariance_matrix[0:2, 0:2]
+    beta = covariance_matrix[2:4, 2:4]
+    gamma = covariance_matrix[0:2, 2:4]
+    i1 = np.linalg.det(alpha)
+    i2 = np.linalg.det(beta)
+    i3 = np.linalg.det(gamma)
     i4 = np.linalg.det(covariance_matrix)
     return [i1, i2, i3, i4]
 
@@ -65,6 +65,17 @@ def symplectic_eigenvalues(covariance_matrix: np.ndarray):
     return d1, d2
 
 
+def _get_symplectic_eigenvalues(cov_or_nus: np.ndarray):
+    if isinstance(cov_or_nus, np.ndarray) and cov_or_nus.shape == (2,):
+        d1, d2 = cov_or_nus
+    elif isinstance(cov_or_nus, np.ndarray) and cov_or_nus.shape == (4, 4):
+        d1, d2 = symplectic_eigenvalues(cov_or_nus)
+    else:
+        print(f'Error in covariance matrix: shape is {input.shape} while 4x4 is expected')
+        raise ValueError
+    return d1, d2
+
+
 def minientropy(d):
     """Ferraro, Olivares, Paris - 2005; pag.22"""
     x1 = d + 1/2
@@ -72,10 +83,67 @@ def minientropy(d):
     return x1*np.log(x1) - x2*np.log(x2)
 
 
-def symplectic_entropy(nus):
+def symplectic_entropy(cov_or_nus: np.ndarray):
     """Ferraro, Olivares, Paris - 2005; pag.22"""
-    return sum([minientropy(nu) for nu in nus if nu != 0])
+    d1, d2 = _get_symplectic_eigenvalues(cov_or_nus)
+    f = minientropy
+    return f(d1) + f(d2)
 
 
-def symplectic_purity(nus):
-    return np.prod([1/nu for nu in nus])
+def symplectic_purity(cov_or_nus: np.ndarray):
+    d1, d2 = _get_symplectic_eigenvalues(cov_or_nus)
+    return 1 / (d1 * d2)
+
+
+def _determinant_minimum(a, b, c, d):
+    left_hand = (d - a*b) ** 2
+    right_hand = (1 + b) * c**2 * (a + d)
+    if left_hand <= right_hand:
+        e_min = 2*c**2 + (-1+b)*(-a+d) + 2*abs(c) * np.sqrt(c**2 + (-1+b)*(-a+d))
+        e_min /= (-1+b) ** 2
+    else:
+        e_min = a*b - c**2 + d - np.sqrt((c**4 + (-a*b+d)**2 - 2*c**2*(a*b+d)))
+        e_min /= 2*b
+    return e_min
+
+
+def gaussian_quantum_discord(covariance_matrix):
+    """
+    @article{PhysRevLett.105.030501,
+      title = {Quantum versus Classical Correlations in Gaussian States},
+      author = {Adesso, Gerardo and Datta, Animesh},
+      journal = {Phys. Rev. Lett.},
+      volume = {105},
+      issue = {3},
+      pages = {030501},
+      numpages = {4},
+      year = {2010},
+      month = {Jul},
+      publisher = {American Physical Society},
+      doi = {10.1103/PhysRevLett.105.030501},
+      url = {https://link.aps.org/doi/10.1103/PhysRevLett.105.030501}
+    }
+    &
+    Su, X. (2013).
+    Applying Gaussian quantum discord to quantum key distribution.
+    ArXiv. /abs/1310.4253
+    """
+    invariants = covariance_invariants(covariance_matrix)
+    d1, d2 = symplectic_eigenvalues(covariance_matrix)
+    e_min = _determinant_minimum(*invariants)
+    f = minientropy
+    return f(np.sqrt(invariants[1])) - f(d1) - f(d2) - f(np.sqrt(e_min))
+
+
+def mutual_information(covariance_matrix):
+    """
+    Serafini, A., Illuminati, F., & De Siena, S. (2003).
+    Symplectic invariants, entropic measures and correlations of Gaussian states.
+    ArXiv. https://doi.org/10.1088/0953-4075/37/2/L02
+    """
+    invariants = covariance_invariants(covariance_matrix)
+    d1, d2 = symplectic_eigenvalues(covariance_matrix)
+    a = np.sqrt(invariants[0])
+    b = np.sqrt(invariants[1])
+    return minientropy(a) + minientropy(b) - minientropy(d1) - minientropy(d2)
+
--- src/rho_evolution_iterD.py
+++ src/rho_evolution_iterD.py
@@ -14,9 +14,10 @@ from physics import *
 from stateobj import Physics
 from observables import entropy_vn, purity, covariance
 
-TIMESTEPS = 200
-TIMEDELTA = 1
+TIMESTEPS = 1000
+TIMEDELTA = 1  # Interaction time
 OMEGA = 0.5  # Strength of Interaction
+D = 20  # Hilbert space dimension
 
 # Ancilla parameters
 A = complex(1 / math.sqrt(6), 0)
@@ -40,10 +41,10 @@ def file_timedelta(filename):
     return float(filename.split('_')[-1][2:])
 
 
-def check_file_metadata(filename, d):
+def check_file_metadata(filename, d, dt):
     dims = file_dims(filename)
     timedelta = file_timedelta(filename)
-    return dims == d and timedelta == TIMEDELTA
+    return dims == d and timedelta == dt
 
 
 def create_systems(alpha, beta, phi, n1, n2, d):
@@ -53,26 +54,13 @@ def create_systems(alpha, beta, phi, n1, n2, d):
     return eta, rho1, rho2
 
 
-def load_or_create(rho1, rho2, dims, create=False):
+def load_or_create(rho1, rho2, dims=D, timedelta=TIMEDELTA, create=False):
     """
     Create a new product state or load it from the last saved evolution file.
     Returns the state and the last time step.
     """
-    if create:
-        return np.kron(rho1.full(), rho2.full()).real, 0
-    try:
-        files = [file.removesuffix('.npz') for file in os.listdir('../objects') if file.endswith('.npz')]
-        times = [file_time(file) for file in files if check_file_metadata(file, dims)]
-        t = max(times) if len(times) > 0 else 0
-        zipped_evolution = np.load(f'../objects/{STATE}/rho_evolution_d{dims}_t{t}_dt{TIMEDELTA}.npz')
-        rho = zipped_evolution[zipped_evolution.files[-1]]
-        del zipped_evolution
-        print(f'Loaded evolution until step {t}.')
-    except FileNotFoundError:
-        rho = np.kron(rho1.full(), rho2.full()).real
-        t = 0
-        print('File not found. Starting a new evolution.')
-    return rho, t
+    suffix = '.npy'
+    return np.kron(rho1.full(), rho2.full()).real, 0
 
 
 def plot_density_matrix(system, diagonal=False, title=None):
@@ -129,9 +117,9 @@ def hilbert_is_good(system, check):
 
 def main(dims=20, timedelta=1):
     p = Physics(dimension=dims, interaction_strength=OMEGA, interaction_time=timedelta)
+    th = OMEGA * timedelta
+    print(f'Starting evolution of {dims}-dimensional system with interaction magnitude {th}')
 
-    print(f'Starting evolution of {dims}-dimensional system')
-    th = OMEGA * TIMEDELTA
     ga = np.real(2 * A ** 2)
     gb = np.real(B ** 2 * (1 + np.cos(PHI)))
 
@@ -170,7 +158,7 @@ def main(dims=20, timedelta=1):
     # Save data
     # np.save(f'../objects/{STATE}/rho_entropy_D{dims}_t{t + 1}_dt{TIMEDELTA}', entropies)
     # np.save(f'../objects/{STATE}/rho_purity_D{dims}_t{t + 1}_dt{TIMEDELTA}', purities)
-    np.save(f'../objects/{STATE}/rho_covariance_D{dims}_t{t+1}_dt{TIMEDELTA}', covariances)
+    np.save(f'../objects/{STATE}/rho_covariance_D{dims}_t{t+1}_dt{timedelta}', covariances)
     # Plot final density matrix
     rho1 = Qobj(rho, dims=[[dims, dims], [dims, dims]]).ptrace(0).full()
     plot_density_matrix(rho1, diagonal=True, title='Final density matrix of the first cavity')
