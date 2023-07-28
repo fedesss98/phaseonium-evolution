665da98cbc947730afdbb812f98e85a2d0721275
Federico Amato <federico.amato1998@gmail.com>
2023-07-28 19:49:43 +0200

    Add symplectic temperatures calculation

--- src/observables.py
+++ src/observables.py
@@ -111,6 +111,20 @@ def _get_symplectic_eigenvalues(cov_or_nus: np.ndarray):
     return d1, d2
 
 
+def symplectic_temperatures(cov_or_nus: np.ndarray):
+    """
+    Ferraro, Olivares, Paris - 2005; pag.19
+    Every Gaussian state can be obtained from a thermal state by a symplectic transformation.
+    This thermal state is a product of thermal states of the single modes,
+    with temperature parameters given by symplectic eigenvalues.
+    """
+    dk = _get_symplectic_eigenvalues(cov_or_nus)
+    k2 = 1 / np.sqrt(2)  # Ferraro, Olivares - 2005 pag.1 - pag.14
+    f = 1 / (4 * k2**2)
+    betak = [np.log((d + 1 + f) / (d - f)) for d in dk]
+    return 1 / betak[0], 1 / betak[1]
+
+
 def minientropy(d):
     """Ferraro, Olivares, Paris - 2005; pag.22"""
     x1 = d + 1/2
