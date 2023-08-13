cb670a747ee4a6d41154f938479e5fe72cbc95ec
Federico Amato <federico.amato1998@gmail.com>
2023-08-13 18:07:21 +0200

    Add new cli options

--- cli.py
+++ cli.py
@@ -11,13 +11,15 @@ def cli():
 
 @cli.command()
 @click.option('--dims', '-d', type=int, default=20, help='Number of dimensions.')
-@click.option('--timedelta', '-dt', type=float, default=0.1, help='Time delta.')
+@click.option('--timedelta', '-dt', type=float, default=0.1, help='Interaction time.')
+@click.option('--omega', type=float, default=1.0, help='Interaction strength.')
 @click.option('--alpha', '-a', type=float, help='Ancilla excited state population')
 @click.option('--phi', '-p', type=float, help='Ancilla ground states phase')
 @click.option('--state', '-s', type=str, default='thermal', help='Cavities state type')
 @click.option('-n1', type=float, default=1, help='Cavity 1 mean photon number')
 @click.option('-n2', type=float, default=1, help='Cavity 2 mean photon number')
 @click.option('--max-timesteps', type=int, default=0, help='Maximum number of timesteps')
+@click.option('--partial', type=int, default=0, help='Save partial evolution')
 def simulate(dims, timedelta, **kwargs):
     """Run the main function with the given dimensions and time delta."""
     main(dims=dims, timedelta=timedelta, **kwargs)
@@ -35,12 +37,14 @@ def iter_dims(timedelta, dims, **kwargs):
 
 @cli.command()
 @click.option('--dims', '-d', type=int, default=20, help='Number of dimensions.')
+@click.option('--omega', type=float, default=1.0, help='Interaction strength.')
 @click.option('--alpha', '-a', type=float, help='Ancilla excited state population')
 @click.option('--phi', '-p', type=float, help='Ancilla ground states phase')
 @click.option('--state', '-s', type=str, default='thermal', help='Cavities state type')
 @click.option('-n1', type=float, default=1, help='Cavity 1 mean photon number')
 @click.option('-n2', type=float, default=1, help='Cavity 2 mean photon number')
 @click.option('--max-timesteps', type=int, default=0, help='Maximum number of timesteps')
+@click.option('--partial', type=int, default=0, help='Save partial evolution')
 @click.argument('timedeltas', nargs=-1)
 def iter_timedeltas(dims, timedeltas, **kwargs):
     """Iterate over time deltas with the given dimensions."""
