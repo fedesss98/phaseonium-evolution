4e69ee8162c9a396e9c418247da43f97096ae46b
Federico Amato <federico.amato1998@gmail.com>
2023-08-24 17:03:37 +0200

    Add stochastic-times evolution

--- cli.py
+++ cli.py
@@ -1,7 +1,8 @@
 import click
 import numpy as np
 
-from src.rho_evolution import main
+from src.rho_evolution import main as rho_evolution
+from src.stochastic_evolution import main as stochastic_evolution
 
 
 @click.group()
@@ -23,7 +24,42 @@ def cli():
 @click.option('-id', type=str, default='000', help='Log ID for the simulation')
 def simulate(dims, timedelta, **kwargs):
     """Run the main function with the given dimensions and time delta."""
-    main(dims=dims, timedelta=timedelta, **kwargs)
+    rho_evolution(dims=dims, timedelta=timedelta, **kwargs)
+
+
+@cli.command()
+@click.option('--dims', '-d', type=int, default=20, help='Number of dimensions.')
+@click.option('--timedelta', '-dt', type=float, default=0.1, help='Average Interaction Time.')
+@click.option('--omega', type=float, default=1.0, help='Interaction strength.')
+@click.option('--alpha', '-a', type=float, help='Ancilla excited state population')
+@click.option('--phi', '-p', type=float, help='Ancilla ground states phase')
+@click.option('--state', '-s', type=str, default='thermal', help='Cavities state type')
+@click.option('-n1', type=float, default=1, help='Cavity 1 mean photon number')
+@click.option('-n2', type=float, default=1, help='Cavity 2 mean photon number')
+@click.option('--max-timesteps', type=int, default=0, help='Maximum number of timesteps')
+@click.option('--partial', type=int, default=0, help='Save partial evolution')
+@click.option('-id', type=str, default='000', help='Log ID for the simulation')
+@click.option('--exact/--not-exact', default=False, help='Log ID for the simulation')
+def stochastic_simulate(dims, timedelta, **kwargs):
+    """Run the main function with the given dimensions and time delta."""
+    stochastic_evolution(dims=dims, timedelta=timedelta, **kwargs)
+
+
+@cli.command()
+@click.option('--dims', '-d', type=int, default=20, help='Number of dimensions.')
+@click.option('--timedelta', '-dt', type=float, default=0.1, help='Interaction time.')
+@click.option('--omega', type=float, default=1.0, help='Interaction strength.')
+@click.option('--alpha', '-a', type=float, help='Ancilla excited state population')
+@click.option('--phi', '-p', type=float, help='Ancilla ground states phase')
+@click.option('--state', '-s', type=str, default='thermal', help='Cavities state type')
+@click.option('-n1', type=float, default=1, help='Cavity 1 mean photon number')
+@click.option('-n2', type=float, default=1, help='Cavity 2 mean photon number')
+@click.option('--max-timesteps', type=int, default=0, help='Maximum number of timesteps')
+@click.option('--partial', type=int, default=0, help='Save partial evolution')
+@click.option('-id', type=str, default='000', help='Log ID for the simulation')
+def simulate(dims, timedelta, **kwargs):
+    """Run the main function with the given dimensions and time delta."""
+    rho_evolution(dims=dims, timedelta=timedelta, **kwargs)
 
 
 @cli.command()
@@ -33,7 +69,7 @@ def simulate(dims, timedelta, **kwargs):
 def iter_dims(timedelta, dims, **kwargs):
     """Iterate over dimensions with the given time delta."""
     for dim in dims:
-        main(dims=dim, timedelta=timedelta, **kwargs)
+        rho_evolution(dims=dim, timedelta=timedelta, **kwargs)
 
 
 @cli.command()
@@ -50,7 +86,7 @@ def iter_dims(timedelta, dims, **kwargs):
 def iter_timedeltas(dims, timedeltas, **kwargs):
     """Iterate over time deltas with the given dimensions."""
     for timedelta in timedeltas:
-        main(dims=dims, timedelta=float(timedelta), **kwargs)
+        rho_evolution(dims=dims, timedelta=float(timedelta), **kwargs)
 
 
 if __name__ == '__main__':
---
+++ src/stochastic_evolution.py
@@ -0,0 +1,82 @@
+try:
+    from rho_evolution import *
+except ModuleNotFoundError:
+    from src.rho_evolution import *
+
+
+def _unitary_evolution(system, physic_object, dt):
+    rho_d = physic_object.dims
+    # Create composite system-ancilla state
+    sigma = tensor(
+        Qobj(system, dims=[[rho_d, rho_d], [rho_d, rho_d]]),
+        physic_object.ancilla
+    )
+    new_rho = unitary_evolution(sigma, dt, physic_object.V1 + physic_object.V2)
+    return new_rho.full()
+
+
+def _meq_evolution(system, physic_object, interaction_time):
+    ga = physic_object.ga
+    gb = physic_object.gb
+    # Create new bosonic operators with stochastic interaction time
+    operators = bosonic_operators(interaction_time, physic_object.a, physic_object.ad)
+    return master_equation(system, ga, gb, operators)
+
+
+def meq_evolution(time, dt_mode, experiment, rho, covariances, heat_transfers, exact_evolution):
+    for t in time:
+        # Extract one interaction time from a Maxwell distribution
+        interaction_time = use.maxwell_extraction(dt_mode)
+        if exact_evolution:
+            rho = _unitary_evolution(rho, experiment, interaction_time)
+        else:
+            delta_rho = _meq_evolution(rho, experiment, interaction_time)
+            rho = rho + delta_rho
+        if not hilbert_is_good(rho, 'unitary'):
+            print(f'Hilbert space truncation is no more valid at step {t}')
+            break
+        else:
+            covariances.append(covariance(rho, experiment.quadratures))
+            # heat_transfers.append(_heat_transfer(delta_rho, experiment))
+    return rho, covariances, heat_transfers
+
+
+def main(dims=20, timedelta=1.0, show_plots=False, **kwargs):
+    print(f'Starting stochastic evolution of {dims}-dimensional system with interaction time {timedelta}.')
+    log_id = kwargs.get('id', '000')
+    experiment = setup_experiment(dims, timedelta, **kwargs)
+    rho1, rho2 = experiment.systems['rho1'], experiment.systems['rho2']
+    # Create new product state and observables or load them evolved until time t
+    rho, covariances, heat_transfers, t = load_or_create(experiment, log_id)
+    if show_plots:
+        plot_density_matrices(rho1, rho2, rho, t)
+
+    # Evolve
+    total_time_range = 2000  # Approximate time to thermalize the cavities
+    timesteps = int(total_time_range / timedelta)
+    max_timesteps = kwargs.get('max_timesteps', 0)
+    if kwargs.get('max_timesteps', 0) == 0:
+        max_timesteps = timesteps
+    time = trange(t, t + max_timesteps)
+    # Evolve density and save observables
+    rho, covariances, heat_transfers = meq_evolution(
+        time, timedelta, experiment, rho, covariances, heat_transfers,
+        kwargs.get('exact', False)
+    )
+
+    save_data(dims, timedelta, t + max_timesteps, covariances, heat_transfers, rho, **kwargs)
+
+    if show_plots:
+        # Trace out evolved cavities
+        rho1 = Qobj(rho, dims=[[dims, dims], [dims, dims]]).ptrace(0).full()
+        rho2 = Qobj(rho, dims=[[dims, dims], [dims, dims]]).ptrace(1).full()
+        plot_density_matrices(rho1, rho2, rho, t)
+
+
+if __name__ == '__main__':
+    d = 17
+    dt = 1.0
+    plots = False
+    partial = 0
+    alpha = complex(1 / np.sqrt(1 + 2*np.e), 0)
+    main(d, dt, plots, partial=partial, max_timesteps=500, alpha=alpha)
